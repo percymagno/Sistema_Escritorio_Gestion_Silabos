94fd513545a9e9fad02ad0fbed53228f9fe41fcd
johan <140156@unsaac.edu.pe>
2021-11-10 20:23:58 -0500

    using sqlclient, capaDatos y capaEntidades agregados

--- CapaPresentacion/P_Director.cs
+++ CapaPresentacion/P_Director.cs
@@ -3,11 +3,14 @@ using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaDatos;
+using CapaEntidades;
 
 namespace CapaPresentacion
 {
