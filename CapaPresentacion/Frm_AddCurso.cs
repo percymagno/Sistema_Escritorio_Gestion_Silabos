4d2ac1052177378e6f46fd1f661e1b113fdfe014
OrlandoCana <89153775+OrlandoCana@users.noreply.github.com>
2021-11-11 18:57:38 -0500

    GDSU-49 Agregar Curso
    agrega la funcionalidad de agregar curso

--- CapaPresentacion/Frm_AddCurso.cs
+++ CapaPresentacion/Frm_AddCurso.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using CapaDatos;
+using CapaEntidades;
 namespace CapaPresentacion
 {
     public partial class Frm_AddCurso : Form
@@ -15,6 +16,23 @@ namespace CapaPresentacion
         public Frm_AddCurso()
         {
             InitializeComponent();
+        }
+
+        private void btn_agregarCurso_Click(object sender, EventArgs e)
+        {
+            E_Curso e_Curso = new E_Curso { CodCurso=text_codigo.Text,
+                                            Nombre=text_nombre.Text,
+                                            Creditos=(Cb_creditos.SelectedIndex+1),
+                                            Categoria=text_categoria.Text};
+            D_Curso d_Curso = new D_Curso();
+            if (d_Curso.AgregarCurso(e_Curso))
+            {
+                MessageBox.Show("Se agrego correctamenete el curso");
+            }
+            else
+            {
+                MessageBox.Show("Error. No se pudo agregar curso");
+            }
         }
     }
 }
--- CapaPresentacion/Frm_Curso.Designer.cs
+++ CapaPresentacion/Frm_Curso.Designer.cs
@@ -31,14 +31,14 @@ namespace CapaPresentacion
         {
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,9 +46,10 @@ namespace CapaPresentacion
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(158, 37);
+            this.label1.Size = new System.Drawing.Size(198, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "CURSOS";
             // 
@@ -61,75 +62,89 @@ namespace CapaPresentacion
             this.Column3,
             this.Column4,
             this.Column5});
-            this.dataGridView1.Location = new System.Drawing.Point(11, 53);
+            this.dataGridView1.Location = new System.Drawing.Point(15, 65);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(768, 388);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1024, 478);
             this.dataGridView1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(502, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 33);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Agregar";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(582, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(80, 33);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Editar";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(668, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(74, 33);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Eliminar";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // Column1
             // 
             this.Column1.HeaderText = "Codigo_Curso";
+            this.Column1.MinimumWidth = 6;
             this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
             // 
             // Column2
             // 
             this.Column2.HeaderText = "Nombre_Curso";
+            this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
             this.Column2.Width = 230;
             // 
             // Column3
             // 
             this.Column3.HeaderText = "Creditos";
+            this.Column3.MinimumWidth = 6;
             this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
             // 
             // Column4
             // 
             this.Column4.HeaderText = "Categoria";
+            this.Column4.MinimumWidth = 6;
             this.Column4.Name = "Column4";
             this.Column4.Width = 200;
             // 
             // Column5
             // 
             this.Column5.HeaderText = "Cod_Docente";
+            this.Column5.MinimumWidth = 6;
             this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(669, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(776, 15);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Editar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(891, 15);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Frm_Curso
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Frm_Curso";
             this.Text = "Frm_Curso";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
