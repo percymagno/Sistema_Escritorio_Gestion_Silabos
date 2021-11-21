36f61d6555ccd22e1b89576f5c76177ad2d8cc22
johan <140156@unsaac.edu.pe>
2021-11-21 10:28:23 -0500

    Clase Excel creada

---
+++ CapaNegocio/Excel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace CapaNegocio
+{
+    public class Excel
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        public Excel(string path, int sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = excel.Worksheets[sheet];
+        }
+        public string ReadCell(int i, int j)
+        {
+            i++; j++;
+            if(ws.Cells[i,j] != null)
+            {
+                return ws.Cells[i,j].Value2;
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
