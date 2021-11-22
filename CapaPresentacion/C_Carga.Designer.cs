c679221b488137a83e4d7eb41f7cd404ec6ca7ab
johan <140156@unsaac.edu.pe>
2021-11-22 13:25:47 -0500

    GDSU-52 cargar archivo excel

--- CapaPresentacion/C_Carga.Designer.cs
+++ CapaPresentacion/C_Carga.Designer.cs
@@ -32,13 +32,15 @@
             this.btnAbrir = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.listBoxCarga = new System.Windows.Forms.ListBox();
+            this.dgvCarga = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarga)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.btnAbrir);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -58,7 +60,7 @@
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.listBoxCarga);
+            this.panel2.Controls.Add(this.dgvCarga);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 50);
             this.panel2.Name = "panel2";
@@ -70,14 +72,18 @@
             this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm|CSV files (*.csv)|*.csv";
             // 
-            // listBoxCarga
+            // dgvCarga
             // 
-            this.listBoxCarga.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBoxCarga.FormattingEnabled = true;
-            this.listBoxCarga.Location = new System.Drawing.Point(0, 0);
-            this.listBoxCarga.Name = "listBoxCarga";
-            this.listBoxCarga.Size = new System.Drawing.Size(800, 400);
-            this.listBoxCarga.TabIndex = 0;
+            this.dgvCarga.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvCarga.BackgroundColor = System.Drawing.Color.White;
+            this.dgvCarga.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCarga.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCarga.Location = new System.Drawing.Point(0, 0);
+            this.dgvCarga.Name = "dgvCarga";
+            this.dgvCarga.ReadOnly = true;
+            this.dgvCarga.RowHeadersVisible = false;
+            this.dgvCarga.Size = new System.Drawing.Size(800, 400);
+            this.dgvCarga.TabIndex = 0;
             // 
             // C_Carga
             // 
@@ -89,6 +95,7 @@
             this.Size = new System.Drawing.Size(800, 450);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarga)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -99,6 +106,6 @@
         private System.Windows.Forms.Button btnAbrir;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.ListBox listBoxCarga;
+        private System.Windows.Forms.DataGridView dgvCarga;
     }
 }
--- CapaPresentacion/C_Carga.cs
+++ CapaPresentacion/C_Carga.cs
@@ -31,9 +31,28 @@ namespace CapaPresentacion
 
                 MessageBox.Show(excel.nroRows().ToString() + " filas leidas");
 
+                Frm_Cargando frm_Cargando = new Frm_Cargando();
+                frm_Cargando.Show();
                 n_Asignacion.Procesar(file);
+                frm_Cargando.Close();
+
+
                 List<Carga> cargas = n_Asignacion.getCargas();
-                listBoxCarga.DataSource = cargas;
+
+                dgvCarga.ColumnCount = 5;
+                dgvCarga.Columns[0].Name = "#";
+                dgvCarga.Columns[1].Name = "Docente";
+                dgvCarga.Columns[2].Name = "CodCurso";
+                dgvCarga.Columns[3].Name = "Grupo";
+                dgvCarga.Columns[4].Name = "Tipo";
+
+                int i = 0;
+                foreach (Carga carga in cargas)
+                {
+                    string[] row = { i.ToString(), carga.Coddocente , carga.CodCurso.Substring(0,5), carga.Grupo, carga.Tipo};
+                    dgvCarga.Rows.Add(row);
+                    i++;
+                }
             }
         }
     }
