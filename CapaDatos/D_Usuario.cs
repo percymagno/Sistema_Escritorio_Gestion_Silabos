784c2b6c7e69b66706267a8790aef56d09f9040b
OrlandoCana <89153775+OrlandoCana@users.noreply.github.com>
2021-11-26 11:36:10 -0500

    csproj cambios

---
+++ CapaDatos/D_Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class D_Usuario
+    {
+        Conexion conexion = new Conexion();
+        // Metodos CRUD
+        public DataTable BuscarUsuario(String pUsuario,String pContrasenia)
+        {
+            string sql = "SELECT CodDocente, Usuario, Acceso FROM TUsuarios WHERE (Usuario = @pUsuario AND Contraseña = @pContrasenia)";
+            conexion.setComando(sql);
+            conexion.cmd.Parameters.AddWithValue("@pUsuario", pUsuario);
+            conexion.cmd.Parameters.AddWithValue("@pContrasenia", pContrasenia);
+            return conexion.executeReader();
+        }
+    }
+}
---
+++ CapaEntidades/E_Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class E_Usuario
+    { 
+        public string Usuario { get; set; }
+        public string Acceso { get; set; }
+    }
+}
