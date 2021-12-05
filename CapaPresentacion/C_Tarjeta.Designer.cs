91a581b36779a7bce1434e6336732e716ccd67d3
Johan <140156@unsaac.edu.pe>
2021-12-05 01:41:20 -0500

    GDSU-86 Control tarjeta creado

---
+++ CapaPresentacion/C_Tarjeta.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CapaPresentacion
+{
+    partial class C_Tarjeta
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnControl = new System.Windows.Forms.Button();
+            this.btnSilabo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(190)))), ((int)(((byte)(210)))));
+            this.lblTitulo.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(0, 0);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(5);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(250, 50);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Planeamiento y direccion estrategica de tecnologias de la informacion";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnControl
+            // 
+            this.btnControl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(170)))), ((int)(((byte)(211)))));
+            this.btnControl.FlatAppearance.BorderSize = 0;
+            this.btnControl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnControl.ForeColor = System.Drawing.Color.White;
+            this.btnControl.Location = new System.Drawing.Point(148, 85);
+            this.btnControl.Name = "btnControl";
+            this.btnControl.Size = new System.Drawing.Size(80, 25);
+            this.btnControl.TabIndex = 1;
+            this.btnControl.Text = "CONTROL";
+            this.btnControl.UseVisualStyleBackColor = false;
+            // 
+            // btnSilabo
+            // 
+            this.btnSilabo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(234)))), ((int)(((byte)(0)))));
+            this.btnSilabo.FlatAppearance.BorderSize = 0;
+            this.btnSilabo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSilabo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSilabo.ForeColor = System.Drawing.Color.White;
+            this.btnSilabo.Location = new System.Drawing.Point(22, 85);
+            this.btnSilabo.Name = "btnSilabo";
+            this.btnSilabo.Size = new System.Drawing.Size(80, 25);
+            this.btnSilabo.TabIndex = 2;
+            this.btnSilabo.Text = "SILABO";
+            this.btnSilabo.UseVisualStyleBackColor = false;
+            // 
+            // C_Tarjeta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.btnSilabo);
+            this.Controls.Add(this.btnControl);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "C_Tarjeta";
+            this.Size = new System.Drawing.Size(250, 120);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnControl;
+        private System.Windows.Forms.Button btnSilabo;
+    }
+}
