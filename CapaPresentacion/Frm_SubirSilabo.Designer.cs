af1e9ddc5781a3f7e0cd6b7bb6213df4d55aefe9
percymagno <70827428+percymagno@users.noreply.github.com>
2021-12-20 19:28:22 -0500

    Update Frm_SubirSilabo.Designer.cs

--- CapaPresentacion/Frm_SubirSilabo.Designer.cs
+++ CapaPresentacion/Frm_SubirSilabo.Designer.cs
@@ -214,7 +214,6 @@ namespace CapaPresentacion
             this.button1.Text = "Agregar";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Frm_SubirSilabo
             // 
