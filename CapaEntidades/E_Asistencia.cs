971e759665333ecd9c282b1c0fc698febe47cc6d
Isac Anderson Usucachi <182936@unsaac.edu.pe>
2022-01-16 17:41:27 -0500

    GDSU-112 Crear las funciones basicas en las capas de datos, entidad y negocio

---
+++ CapaEntidades/E_Asistencia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class E_Asistencia
+    {
+        public int ID { get; set; }
+        public int ID_Registro { get; set; }
+        public string CodAlumno { get; set; }
+        public bool Asistio { get; set; }
+    }
+}
---
+++ CapaNegocio/N_RegistroAvance.cs
@@ -0,0 +1,62 @@
+﻿using CapaDatos;
+using CapaEntidades;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public class N_RegistroAvance
+    {
+        public int ID { get; set; }
+        public int ID_Silabo { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "ID_Silabo requerido")]
+        public DateTime Fecha { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fecha es requerido")]
+        public string Observacion { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Observacion es requerido")]
+
+        D_RegistroAvance d_RegistroAvance = new D_RegistroAvance();
+
+        public override string ToString()
+        {
+            return "ID_Silabo: " + ID_Silabo + ", Fecha: " + Fecha + ", Observacion: " + Observacion;
+        }
+
+        public int Guardar()
+        {
+            E_RegistroAvance e_RegistroAvance = new E_RegistroAvance
+            {
+                ID_Silabo = this.ID_Silabo,
+                Fecha = this.Fecha,
+                Observacion = this.Observacion,
+            };
+
+            Console.WriteLine("Guardando: " + this.ToString());
+            d_RegistroAvance.Agregar(e_RegistroAvance);
+            return 1;
+        }
+        public DataTable Mostrar(int AsignacionID)
+        {
+            return d_RegistroAvance.MostrarAsignacion(AsignacionID);
+        }
+        public DataTable Buscar(int AsignacionID, string texto)
+        {
+            return d_RegistroAvance.Buscar(AsignacionID, texto);
+        }
+        public bool Editar()
+        {
+            return d_RegistroAvance.Editar(new E_RegistroAvance
+            {
+                ID = this.ID,
+                ID_Silabo = this.ID_Silabo,
+                Fecha = this.Fecha,
+                Observacion = this.Observacion,
+            });
+        }
+        public bool Eliminar(string ID)
+        {
+            return d_RegistroAvance.Eliminar(ID);
+        }
+    }
+}
